563694447b7d5ebd20cbe9f2cc644e1aa5611c03
Yonsm <I@YONSM.NET>
2017-05-23 17:09:03 +0800

    add fish hook

--- Piercing/Sources/Main.mm
+++ Piercing/Sources/Main.mm
@@ -1,11 +1,29 @@
 
 //
-#if __cplusplus
-extern "C"
-#endif
+#import <objc/runtime.h>
+#import "Prefix.pch"
+#import <dlfcn.h>
+#import "fishhook.h"
 
-__attribute__((constructor)) int main()
+static void * (*orig_dlsym)(void *, const char *);
+
+int my_ptrace(int _request, pid_t _pid, caddr_t _addr, int _data)
+{
+	return 0;
+}
+
+void * my_dlsym(void * __handle, const char * __symbol)
+{
+	if (strcmp(__symbol, "ptrace") == 0) {
+		//return &my_ptrace;
+	}
+	
+	return orig_dlsym(__handle, __symbol);
+}
+
+__attribute__((constructor)) void init()
 {
 	_LogLine();
-	return 0;
+	//orig_dlsym = dlsym(RTLD_DEFAULT, "dlsym");
+	//rebind_symbols((struct rebinding[1]){{"dlsym", my_dlsym}}, 1);
 }
