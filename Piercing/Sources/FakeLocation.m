e4e47e7906f1ed032d5c0fb54cc7b3a1cd109ff4
Yonsm <I@YONSM.NET>
2017-07-03 22:59:09 +0800

    General Location

--- Piercing/Sources/FakeLocation.m
+++ Piercing/Sources/FakeLocation.m
@@ -4,66 +4,71 @@
 #import <objc/runtime.h>
 #import <CoreLocation/CoreLocation.h>
 
-//
-//HOOK_MESSAGE(void, CLLocationManager, onClientEventLocation_, id foo)
-//{
-//	// no-op - this suppresses location change events that are raised by CLLocationManager
-//}
-//
-////
-//HOOK_MESSAGE(void, CLLocationManager, startUpdatingLocation)
-//{
-//	// no-op - this suppresses location change events that are raised by CLLocationManager
-//	[(CLLocationManager *)self setDelegate:(id<CLLocationManagerDelegate> _Nullable)]
-//}
 
-//
-void $CLLocationManagerDelegateFaker_locationManager_didUpdateLocations_(NSObject *self, SEL sel, CLLocationManager *manager, NSArray<CLLocation *> *locations)
+NS_INLINE CLLocation *FakeLocation(CLLocation *location)
 {
-	void (*old)(id self, SEL sel, CLLocationManager *manager, NSArray<CLLocation *> *locations) = [objc_getAssociatedObject(self.class, @"didUpdateLocationsHook") pointerValue];
-	CLLocation *location = [[CLLocation alloc] initWithLatitude:27.840109 longitude:121.157791];
-	old(self, sel, manager, @[location]);
+	return [[CLLocation alloc] initWithCoordinate:(CLLocationCoordinate2D){27.840109, 121.157791}
+										 altitude:location.altitude
+							   horizontalAccuracy:location.horizontalAccuracy
+								 verticalAccuracy:location.verticalAccuracy
+										timestamp:location.timestamp];
 }
 
 //
-void $CLLocationManagerDelegateFaker_locationManager_didUpdateToLocation_fromLocation_(NSObject *self, SEL sel, CLLocationManager *manager, CLLocation *location, CLLocation *from)
+#if _FOR_WECHAT_ONLY
+
+//
+HOOK_MESSAGE(void, MMLocationMgr, locationManager_didUpdateToLocation_fromLocation_, CLLocationManager *manager, CLLocation *location, CLLocation *from)
 {
-	void (*old)(id self, SEL sel, CLLocationManager *manager, CLLocation *location, CLLocation *from) = [objc_getAssociatedObject(self.class, @"didUpdateToLocationHook") pointerValue];
+	_MMLocationMgr_locationManager_didUpdateToLocation_fromLocation_(self, sel, manager, FakeLocation(location), nil);
+}
+
+//
+HOOK_MESSAGE(void, QMapView, locationManager_didUpdateToLocation_fromLocation_, CLLocationManager *manager, CLLocation *location, CLLocation *from)
+{
+	_QMapView_locationManager_didUpdateToLocation_fromLocation_(self, sel, manager, FakeLocation(location), nil);
+}
+
+#else
+
+
+void $CLLocationManagerDelegate_locationManager_didUpdateLocations_(NSObject *self, SEL sel, CLLocationManager *manager, NSArray<CLLocation *> *locations)
+{
+	void (*old)(id self, SEL sel, CLLocationManager *manager, NSArray<CLLocation *> *locations) = [objc_getAssociatedObject(self.class, @"_locationManager_didUpdateToLocation_fromLocation_") pointerValue];
 	
-	CLLocation *location2 = [[CLLocation alloc] initWithLatitude:27.840109 longitude:121.157791];
-	//location2.altitude = 65;
-	old(self, sel, manager, location2, nil);
+	old(self, sel, manager, locations.count ? @[FakeLocation(locations[0])] : nil);
+}
+
+//
+void $CLLocationManagerDelegate_locationManager_didUpdateToLocation_fromLocation_(NSObject *self, SEL sel, CLLocationManager *manager, CLLocation *location, CLLocation *from)
+{
+	void (*old)(id self, SEL sel, CLLocationManager *manager, CLLocation *location, CLLocation *from) = [objc_getAssociatedObject(self.class, @"_locationManager_didUpdateToLocation_fromLocation_") pointerValue];
+	old(self, sel, manager, FakeLocation(location), nil);
 }
 
 //
 HOOK_MESSAGE(void, CLLocationManager, setDelegate_, id<CLLocationManagerDelegate> delegate)
 {
-	//dispatch_async(dispatch_get_main_queue(), ^{
-	if ([delegate respondsToSelector:@selector(locationManager:didUpdateLocations:)])
+	if (!objc_getAssociatedObject(delegate.class, @"_locationManager_didUpdateToLocation_fromLocation_"))
 	{
-		if (objc_getAssociatedObject(delegate.class, @"didUpdateLocationsHook") == nil)
+		Method method = class_getInstanceMethod(delegate.class, @selector(locationManager:didUpdateToLocation:fromLocation:));
+		if (method)
 		{
-			Method method = class_getInstanceMethod(delegate.class, @selector(locationManager:didUpdateToLocation:fromLocation:));
-			if (method)
-			{
-				IMP old = method_setImplementation(method, (IMP)$CLLocationManagerDelegateFaker_locationManager_didUpdateToLocation_fromLocation_);
-				objc_setAssociatedObject(delegate.class, @"didUpdateLocationsHook", [NSValue valueWithPointer:old], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-			}
+			IMP old = method_setImplementation(method, (IMP)$CLLocationManagerDelegate_locationManager_didUpdateToLocation_fromLocation_);
+			objc_setAssociatedObject(delegate.class, @"_locationManager_didUpdateToLocation_fromLocation_", [NSValue valueWithPointer:old], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 		}
 	}
-	if ([delegate respondsToSelector:@selector(locationManager:didUpdateToLocation:fromLocation:)])
+	if (!objc_getAssociatedObject(delegate.class, @"_locationManager_didUpdateToLocation_fromLocation_"))
 	{
-		if (objc_getAssociatedObject(delegate.class, @"didUpdateToLocationHook") == nil)
+		Method method = class_getInstanceMethod(delegate.class, @selector(locationManager:didUpdateToLocation:fromLocation:));
+		if (method)
 		{
-			Method method = class_getInstanceMethod(delegate.class, @selector(locationManager:didUpdateToLocation:fromLocation:));
-			if (method)
-			{
-				IMP old = method_setImplementation(method, (IMP)$CLLocationManagerDelegateFaker_locationManager_didUpdateToLocation_fromLocation_);
-				objc_setAssociatedObject(delegate.class, @"didUpdateToLocationHook", [NSValue valueWithPointer:old], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-			}
+			IMP old = method_setImplementation(method, (IMP)$CLLocationManagerDelegate_locationManager_didUpdateToLocation_fromLocation_);
+			objc_setAssociatedObject(delegate.class, @"_locationManager_didUpdateToLocation_fromLocation_", [NSValue valueWithPointer:old], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 		}
 	}
-	//});
 	
 	_CLLocationManager_setDelegate_(self, sel, delegate);
 }
+
+#endif
